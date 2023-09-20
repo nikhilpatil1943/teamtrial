e24a0961594a3cfe74b4d195c2325921a9941cc1
Nikhil Patil <nikhil.patil_entc21@pccoer.in>
2023-09-20 20:34:42 +0530

    hello_Api

--- server/index.js
+++ server/index.js
@@ -1,1 +1,31 @@
-console.log("hello world")
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.get('/', (req,res)=>{
+    res.send('Hello World');
+    })
+
+
+
+
+const PORT = process.env.PORT || 5000;
+const DB = process.env.DB;
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    app.listen(PORT, () => console.log(` connected to data-base and Server Started at PORT ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
