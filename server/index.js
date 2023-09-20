ec61fe90436b71068bc320a52d81469441249ccc
Nikhil Patil <nikhil.patil_entc21@pccoer.in>
2023-09-20 20:51:47 +0530

    created_api_1

--- server/index.js
+++ server/index.js
@@ -9,14 +9,13 @@ dotenv.config();
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.get('/api1', (req,res)=>{
+    res.send('Hello this is Team Trial');
+    })
 
 app.get('/', (req,res)=>{
     res.send('Hello World');
     })
-
-
-
 
 const PORT = process.env.PORT || 5000;
 const DB = process.env.DB;
